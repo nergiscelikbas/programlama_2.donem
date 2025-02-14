c00b1d97c974539e8f6c9671f8ab378c1c67babc
nergiscelikbas <nergiscelikbas0@gmail.com>
2025-02-14 12:03:09 +0300

    Add files via upload

---
+++ 250214/ornek1.py
@@ -0,0 +1,5 @@
+from email.charset import add_alias
+
+
+asal_sayilar=[2,3,5,7,11,13,17,19,23]
+print(asal_sayilar[5:])
---
+++ 250214/ornek5.py
@@ -0,0 +1,8 @@
+tek_sayilar=[3,5,7,9,11,13,15,17,19,]
+print(tek_sayilar[0:6])
+print(tek_sayilar[2:5])
+print(tek_sayilar[3:8])
+print(tek_sayilar[:5])
+print(tek_sayilar[3:])
+print(tek_sayilar[0:8:2])
+print(tek_sayilar[::3])
