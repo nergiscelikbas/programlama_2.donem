1a1cabc04d3047189c728840f74bd65bbac06db1
nergiscelikbas <nergiscelikbas0@gmail.com>
2025-03-28 11:59:50 +0300

    Add files via upload

---
+++ 250314/250328/ornek1.py
@@ -0,0 +1,6 @@
+islemci=input("işlemci adını girin :")
+rambellek=int(input("kaç gb girin :"))
+if islemci=="i7"and rambellek==8:
+    print("kurulum uygun")
+else:
+    print("kurulum uygun değil")
---
+++ 250314/250328/ornek2.py
@@ -0,0 +1,5 @@
+sicaklik=int(input("hava sıcaklığını girin:"))
+if sicaklik<=4:
+    print("dikkat buzlanma tehlikesi")
+else:
+    print("iyi yolculuklar")
